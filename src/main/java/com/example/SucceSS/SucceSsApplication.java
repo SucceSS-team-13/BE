2f4161edc0679366548b9980c1a0e4c55e42aa43
ruddmslee <dlruddms326@naver.com>
2025-01-27 22:37:43 +0900

    Initial Commit

---
+++ src/main/java/com/example/SucceSS/SucceSsApplication.java
@@ -0,0 +1,13 @@
+package com.example.SucceSS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SucceSsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SucceSsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SucceSS/SucceSsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SucceSS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SucceSsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
