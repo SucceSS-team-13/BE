a7094e6ea17c3b4bd9bb845533383717ec50d831
ouob123 <kyjin320@sookmyung.ac.kr>
2025-02-12 21:33:38 +0900

    [FIX] Member Entity 수정

--- src/main/java/com/example/SucceSS/domain/Member.java
+++ src/main/java/com/example/SucceSS/domain/Member.java
@@ -48,44 +48,4 @@ public class Member extends BaseEntity {
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<MemberHobby> memberHobbies = new ArrayList<>();
 
-    /*@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="member_id")
-    private Long id;
-
-    @Column(name="social_id", unique = true)
-    private String socialId;
-
-    // @NotNull
-    @Column(name = "nickname")
-    private String nickname;
-
-    @NotNull
-    @Enumerated(EnumType.STRING)
-    @Column(name="user_role")
-    private Role userRole;
-
-    @Enumerated(EnumType.STRING)
-    private AgeGroup ageGroup;
-
-    private String location;
-
-    @Enumerated(EnumType.STRING)
-    private PersonalityEnergy personalityEnergy;
-
-    @Enumerated(EnumType.STRING)
-    private PersonalityJudgement personalityJudegement;
-
-    // Hobbies 컬렉션 필드 추가
-    @ElementCollection(targetClass = Hobby.class)
-    @CollectionTable(name = "member_hobby", joinColumns = @JoinColumn(name = "member_id"))
-    @Enumerated(EnumType.STRING)
-    private List<Hobby> hobby = new ArrayList<>();
-
-    // Detailed Hobbies 컬렉션 필드 추가
-    @ElementCollection(targetClass = DetailedHobby.class)
-    @CollectionTable(name = "member_detailed_hobby", joinColumns = @JoinColumn(name = "member_id"))
-    @Enumerated(EnumType.STRING)
-    private List<DetailedHobby> detailedHobby = new ArrayList<>();*/
-
 }
