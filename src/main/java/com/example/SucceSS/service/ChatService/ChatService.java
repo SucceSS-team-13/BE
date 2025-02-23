a46c365348073daec1acf64d36b49561b41acba7
ruddmslee <dlruddms326@naver.com>
2025-02-23 11:01:51 +0900

    Feat: 통합된 AI 모델 API 호출

--- src/main/java/com/example/SucceSS/service/ChatService/ChatService.java
+++ src/main/java/com/example/SucceSS/service/ChatService/ChatService.java
@@ -10,11 +10,11 @@ import com.example.SucceSS.repository.ChatRepository;
 import com.example.SucceSS.repository.ChatRoomRepository;
 import com.example.SucceSS.web.dto.ChatResponseDto;
 import com.example.SucceSS.web.dto.ChatRequestDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,7 +23,6 @@ import org.springframework.web.client.RestTemplate;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -37,66 +36,64 @@ public class ChatService {
     private static final String USER = "user";
     private static final String LUMI = "lumi";
 
-    @Value("${spring.huggingface.api.url}")
-    private String API_URL;
-
-    @Value("${spring.huggingface.api.key}")
-    private String API_KEY;
+    private static final String API_URL = "https://Chanjeans-vector.hf.space/chat_or_recommend";
 
     @Transactional
     public ChatResponseDto userSendChat(ChatRequestDto chatDto, Member member) throws IOException {
         updateChatRoomInfos(chatRoomRepository.findByChatRoomId(chatDto.getChatRoomId())
                 .orElseThrow(ChatRoomNotFound::new), chatDto);
 
-        Chat newChat = chatRepository.save(Chat.of(chatDto,member.getId()));
+        chatRepository.save(Chat.of(chatDto,member.getId()));
 
         Long chatRoomId = chatDto.getChatRoomId();
         String message = chatDto.getText();
-        Map<String, Object> createdProfiles = createUserProfile(member);
-        Map<String, Object> userProfile = null;
-        if (createdProfiles.get("userProfile") instanceof Map) {
-            userProfile = (Map<String, Object>) createdProfiles;
-        } else {
-            log.error("Expected Map but got: " + createdProfiles.getClass().getName());
-        }
 
-        String response = generateAiResponse(message, createdProfiles.get("mode").toString(), userProfile);
+        String createdPayload = createPayload(member, message);
+
+        String response = generateAiResponse(createdPayload);
         Chat aiResponse = chatRepository.save(Chat.of(chatRoomId, member.getId(), response, LUMI));
 
         return ChatResponseDto.fromWithoutLocation(aiResponse);
     }
 
-    private String generateAiResponse(String userInput, String mode, Map<String, Object> userProfile) throws IOException {
-        String prompt = String.format("<%s><usr>%s</usr><sys>", mode, userInput);
+    private String generateAiResponse(String payload) throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
-        // Hugging Face API 요청을 위한 payload 생성
-        String jsonPayload = createPayload(prompt, objectMapper, userProfile, userInput);
-
         RestTemplate restTemplate = new RestTemplate();
+
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.set("Authorization", "Bearer " + API_KEY);
 
-        HttpEntity<String> requestEntity = new HttpEntity<>(jsonPayload, headers);
-        ResponseEntity<String> responseEntity = restTemplate.exchange(API_URL, HttpMethod.POST, requestEntity, String.class);
+        ResponseEntity<String> responseEntity = restTemplate.postForEntity(API_URL, new HttpEntity<>(payload, headers), String.class);
 
         if (responseEntity.getStatusCode() == HttpStatus.OK) {
             String response = responseEntity.getBody();
             JsonNode node = objectMapper.readTree(response);
-            node = node.get(0).get("generated_text");
-            if (node != null) {
-                response = node.asText().replace(prompt, "").replace("<sys>", "").replace("</sys>", "").trim();
-                int lastIndex = response.lastIndexOf(".");
-                if (lastIndex != -1) { response = response.substring(0, lastIndex + 1); }
-            }
-            else { return "no generated_text"; }
-            return response;
+            return getCleanResponse(node);
         }
         else { return "Error : " + responseEntity.getStatusCode(); }
     }
 
-    private static Map<String, Object> createUserProfile(Member member) {
+    private static String getCleanResponse(JsonNode node) {
+        String mode = node.get("mode").asText();
+        String response = null;
+        if (mode.equals("recommend") || mode.equals("chat")) {
+            node = node.get("response");
+            if (node != null) {
+                response = node.asText().replace("\"", "").replace("<sys>", "").replace("</sys>", "").trim();
+                if (mode.equals("chat")) {
+                    int lastIndex = response.lastIndexOf(".");
+                    if (lastIndex != -1) { response = response.substring(0, lastIndex + 1); }
+                }
+                return response;
+            }
+            else { return "no generated_text"; }
+        }
+        else { return "Error: 잘못된 응답 모드입니다."; }
+    }
+
+    private static String createPayload(Member member, String message) throws JsonProcessingException {
         // AI 모델에 전달할 userProfile 생성
+        ObjectMapper objectMapper = new ObjectMapper();
         String mode = null;
         String modeDes = null;
         if (member.getPersonalityJudgement() == PersonalityJudgement.EMOTIONAL) {
@@ -108,7 +105,6 @@ public class ChatService {
         else
             log.error("잘못된 감정 형식입니다.");
 
-        Map<String, Object> userProfile = new HashMap<>();
         List<String> hobby = new ArrayList<>();
         List<String> detailedHobby = new ArrayList<>();
         List<MemberHobby> memberHobbies = member.getMemberHobbies();
@@ -127,38 +123,8 @@ public class ChatService {
             energyDes = member.getPersonalityEnergy().getDescription() + "형";
         else { log.error("잘못된 에너지 형식입니다."); }
 
-        userProfile.put("hobby", hobby);
-        userProfile.put("detailed_hobby", detailedHobby);
-        userProfile.put("extroversion", modeDes);
-        userProfile.put("feeling_thinking", energyDes);
-
-        Map<String, Object> resultMap = new HashMap<>();
-        resultMap.put("mode", mode);
-        resultMap.put("userProfile", userProfile);
-        return resultMap;
-    }
-
-
-    private static String createPayload(String prompt, ObjectMapper objectMapper, Map<String, Object> userProfile, String userInput) throws IOException {
-        Map<String, Object> generationParams = new HashMap<>();
-        generationParams.put("max_new_tokens", 128);
-        generationParams.put("temperature", 0.7);
-        generationParams.put("top_p", 0.9);
-        generationParams.put("top_k", 50);
-        generationParams.put("repetition_penalty", 1.2);
-        generationParams.put("do_sample", true);
-
-        Map<String, Object> options = new HashMap<>();
-        options.put( "wait_for_model", true);
-
-        Map<String, Object> payload = new HashMap<>();
-        payload.put("inputs", prompt);
-        payload.put("parameters", generationParams);
-        payload.put("options", options);
-        payload.put("user_profile", userProfile);
-        payload.put("user_input", userInput);
-
-        return objectMapper.writeValueAsString(payload);
+         return objectMapper.writeValueAsString(Map.of("user_input", message, "mode", mode, "extroversion", energyDes,
+                       "feeling_thinking", modeDes, "hobby", hobby, "detail_hobby", detailedHobby));
     }
 
     public void updateChatRoomInfos(ChatRoom chatRoom, ChatRequestDto chatDto) {
